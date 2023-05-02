456e1878e8ec9fd649387449092126980299a071
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-05-02 22:27:39 +0630

    use routes

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/htetoonaing/go-gin-gorm-postgress-docker-rest-api/routes"
+)
 
 func main() {
 	router := gin.New()
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.String(200, "Hello World")
-	})
+	// router.GET("/", func(ctx *gin.Context) {
+	// 	ctx.String(200, "Hello World")
+	// })
+
+	routes.GetUsers(router)
 
 	router.Run(":8080")
 }
