e1ad2f70323a4e61243c8385cecb5a3318384a52
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-05-11 23:53:40 +0630

    connect with postgress and auto migrate user

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/htetoonaing/go-gin-gorm-postgress-docker-rest-api/config"
 	"github.com/htetoonaing/go-gin-gorm-postgress-docker-rest-api/routes"
 )
 
@@ -11,6 +12,8 @@ func main() {
 	// router.GET("/", func(ctx *gin.Context) {
 	// 	ctx.String(200, "Hello World")
 	// })
+
+	config.Connect()
 
 	routes.GetUsers(router)
 
