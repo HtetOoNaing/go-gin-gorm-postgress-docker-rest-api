85bfdc0277798563cdf0b549cc1593db1cf0b5f2
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-05-11 23:11:36 +0630

    add user model

--- config/db.go
+++ config/db.go
@@ -1,16 +1,19 @@
 package config
 
 import (
-	"fmt"
-
+	"github.com/htetoonaing/go-gin-gorm-postgress-docker-rest-api/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
 	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err == nil {
-		fmt.Println("db", db)
+	if err != nil {
+		panic(err)
 	}
+	db.AutoMigrate(&models.User{})
+	DB = db
 }
